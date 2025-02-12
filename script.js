8a72db59d2a7d85dd5960e86585d526b7ae1092e
Conroy95 <leandervdneut@ziggo.nl>
2025-02-12 18:43:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 <h2>${auto.Merk} ${auto.Type}</h2>
                 <p><strong>Schaal:</strong> ${auto.Schaal}</p>
                 <p>${auto.Omschrijving}</p>
-                <p><strong>Categorie:</strong> <p class="label">${auto.Categorie}</p></p>
+                <p><strong>Categorie:</strong> <p class="label ${auto.Categorie}">${auto.Categorie}</p></p>
                 <a class="ui red fa fa-tag label">Red</a>
                 <p><strong>Prijs:</strong> ${auto.Prijs}</p>
             `;
