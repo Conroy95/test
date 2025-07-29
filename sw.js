c423069f9e3188c7739b38b1d7894633e4f014a1
Conroy95 <leandervdneut@ziggo.nl>
2025-07-29 16:32:51 +0200

    Update

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
 const CACHE_NAME = 'roadtrip-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json'
-];
+const urlsToCache = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
